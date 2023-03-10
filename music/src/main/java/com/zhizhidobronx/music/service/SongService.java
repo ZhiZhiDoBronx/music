bbb6a38f89e71f1b2b6e9eb810807e3716147ff2
zhizhi <1198314523@qq.com>
2023-03-10 11:11:23 +0800

    test

--- music/src/main/java/com/zhizhidobronx/music/service/SongService.java
+++ music/src/main/java/com/zhizhidobronx/music/service/SongService.java
@@ -1,29 +1,28 @@
-package com.zhizhidobronx.music.dao;
+package com.zhizhidobronx.music.service;
 
+import com.zhizhidobronx.music.domain.Singer;
 import com.zhizhidobronx.music.domain.Song;
-import org.apache.ibatis.annotations.Mapper;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
-@Mapper
-public interface SongMapper {
-
+/**
+ * 歌曲service接口
+ */
+public interface SongService {
     /**
      *增加
      */
-    public int insert(Song song);
+    public boolean insert(Song song);
 
     /**
      *修改
      */
-    public int update(Song song);
+    public boolean update(Song song);
 
     /**
      * 删除
      */
-    public int delete(Integer id);
+    public boolean delete(Integer id);
 
     /**
      * 根据主键查询整个对象
@@ -31,17 +30,17 @@ public interface SongMapper {
     public Song selectByPrimaryKey(Integer id);
 
     /**
-     * 查询所有歌曲
+     * 查询所有歌手
      */
     public List<Song> allSong();
 
     /**
-     * 根据歌名模糊查询列表
+     * 根据歌手名字模糊查询列表
      */
     public List<Song> songOfName(String name);
 
     /**
-     * 根据歌手id查询
+     * 根据性别查询
      */
     public List<Song> songOfSingerId(Integer SingerId);
 }
