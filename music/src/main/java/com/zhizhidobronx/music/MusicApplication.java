92c4a7e44673c98201af8d2bd6690dca357bf2e4
zhizhi <1198314523@qq.com>
2023-03-13 22:54:39 +0800

    2023/3/13

--- music/src/main/java/com/zhizhidobronx/music/MusicApplication.java
+++ music/src/main/java/com/zhizhidobronx/music/MusicApplication.java
@@ -1,5 +1,8 @@
 package com.zhizhidobronx.music;
 
+import lombok.extern.log4j.Log4j2;
+import org.apache.ibatis.logging.LogFactory;
+import org.apache.ibatis.logging.stdout.StdOutImpl;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,10 +12,13 @@ import java.io.File;
 
 @SpringBootApplication
 @MapperScan("com.zhizhidobronx.music.dao")
+@Log4j2
 public class MusicApplication {
 
     public static void main(String[] args) {
+        LogFactory.useCustomLogging(StdOutImpl.class);
         SpringApplication.run(MusicApplication.class, args);
+        log.info("springboot start!!!");
     }
 
 
